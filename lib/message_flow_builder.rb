fac567792fb6025d9db3fd5c6a97a5769163c8a7
Dan Polyuha <dpolyuha@gmail.com>
2017-07-08 23:28:18 +0300

    MessageFlowBuilder and it's spec implemented.

--- lib/message_flow_builder.rb
+++ lib/message_flow_builder.rb
@@ -7,7 +7,9 @@ class MessageFlowBuilder
   def initialize user, &block
     self.messages = {}
     self.user = user
+    self.flow = {}
     instance_exec(&block)
+    assign_next_messages
   end
 
   def first_message
@@ -16,10 +18,11 @@ class MessageFlowBuilder
 
   private
 
-  attr_accessor :user, :messages
+  attr_accessor :user, :messages, :flow
 
-  def message key, text:, answer_pattern: nil, assigner: nil
+  def message key, text:, answer_pattern: nil, assigner: nil, next_message: nil
     messages[key] = create_message(text, answer_pattern, assigner)
+    flow[key] = next_message if next_message
   end
 
   def create_message text, answer_pattern, assigner
@@ -29,11 +32,28 @@ class MessageFlowBuilder
                 user: user
   end
 
-  # def generate_proc next_messages
-  #   ->(user) do
-  #     key = next_messages.call(user)
-  #     messages[key]
-  #   end
-  # end
-  #
+  def assign_next_messages
+    flow.each do |message_key, next_message_key|
+      message = messages[message_key]
+      next_message = next_message_by_key(next_message_key)
+      message.next_message_resolver = Caller.new(next_message)
+    end
+  end
+
+  def next_message_by_key key
+    return wrap_next_message_proc(key) if key.is_a?(Proc)
+
+    next_message = messages[key]
+    raise RuntimeError, "#{key} is incorrect next message key" unless next_message
+
+    next_message
+  end
+
+  def wrap_next_message_proc proc
+    ->(user) do
+      key = proc.call(user)
+      messages[key]
+    end
+  end
+
 end
--- spec/lib/message_flow_builder_spec.rb
+++ spec/lib/message_flow_builder_spec.rb
@@ -32,6 +32,100 @@ RSpec.describe MessageFlowBuilder do
       end
     end
 
+    context "when answer pattern is regexp" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                answer_pattern: /^yes|no$/
+      }}
+
+      it "creates message with corresponding answer pattern" do
+        expect(builder.first_message.process_answer("yes").success?).to be_truthy
+        expect(builder.first_message.process_answer("maybe").success?).to be_falsey
+      end
+    end
+
+    context "when answer pattern is proc" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                answer_pattern: ->(user) { /^Paul|#{user.name}$/ }
+      }}
+
+      it "creates message with corresponding answer pattern" do
+        expect(builder.first_message.process_answer("John").success?).to be_truthy
+        expect(builder.first_message.process_answer("George").success?).to be_falsey
+      end
+    end
+
+    context "when assigner is symbol" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                assigner: :name
+      }}
+
+      it "assigns answer to corresponding attribute" do
+        expect{builder.first_message.process_answer("George")}.to change{user.name}.from("John").to("George")
+      end
+    end
+
+    context "when assigner is proc" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                assigner: ->(user, answer) { user.name = answer }
+      }}
+
+      it "assigns answer to corresponding attribute" do
+        expect{builder.first_message.process_answer("George")}.to change{user.name}.from("John").to("George")
+      end
+    end
+
+    context "when next_message is message key" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                next_message: :message2
+
+        message :message2,
+                text: "good bye"
+      }}
+
+      it "creates and correctly assigns next message" do
+        next_message = builder.first_message.process_answer("hi").next_message
+        expect(next_message.get_text).to eq("good bye")
+      end
+    end
+
+    context "when next_message is wrong message key" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                next_message: :message2
+      }}
+
+      it "raises exception" do
+        expect{builder.first_message.process_answer("hi").next_message}.to raise_error(RuntimeError)
+      end
+    end
+
+    context "when next_message is proc" do
+      let(:scenario) { ->{
+        message :message1,
+                text: "hello",
+                next_message: ->(user){user.name.downcase.to_sym}
+
+        message :john,
+                text: "good bye"
+      }}
+
+      it "creates and correctly assigns next message" do
+        next_message = builder.first_message.process_answer("hi").next_message
+        expect(next_message.get_text).to eq("good bye")
+      end
+    end
+
   end
 
 end
